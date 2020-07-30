d3c98a4515df3d803f807c807fbf40bb79c8cb04
Artiya Thinkumpang <artiya4u@gmail.com>
2020-07-30 20:52:34 +0700

    Update to remove header from article click from the base website.

--- content.js
+++ content.js
@@ -1,6 +1,16 @@
+const hackernoonBaseUrl = ["https://hackernoon.com/", "https://www.hackernoon.com/"];
+
 let makeReadable = function () {
-  let header = document.querySelector('header');
-  header.parentNode.removeChild(header);
+  setTimeout(function () {
+    let currentURL = window.location.href;
+    if (hackernoonBaseUrl.indexOf(currentURL) < 0) { // Is article url.
+      let header = document.querySelector('header');
+      if (header) {
+        header.parentNode.removeChild(header);
+      }
+    }
+    makeReadable();
+  }, 1000);
 };
 
 makeReadable();
